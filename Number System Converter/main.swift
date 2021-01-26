1eb288e1e3f4249de2b5e962d72f526e92a0151b
Devon Hanses <dhansen@lcs.on.ca>
2021-01-26 14:36:43 -0500

    Initial Commit

---
+++ Number System Converter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Number System Converter
+//
+//  Created by Devon Hansen on 2021-01-26.
+//  Copyright © 2021 Devon Hansen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
